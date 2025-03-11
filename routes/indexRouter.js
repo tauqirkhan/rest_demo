6cb366395a0d4199c3ae8e41d56a43d808a2ffea
Tauqir <tauqirkhann.11@gmail.com>
2025-03-11 16:45:31 +0300

    fix: replace 'app' obj with 'indexRouter' obj

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,20 +4,20 @@ const indexRouter = Router();
 
 const users = [{ email: "abc@foo.com" }];
 
-app.get("/articles", (req, res) => {
+indexRouter.get("/articles", (req, res) => {
   const articles = [];
   // code to retrieve an article...
   res.json(articles);
 });
 
-app.get("/articles/:articlesId/comments", (req, res) => {
+indexRouter.get("/articles/:articlesId/comments", (req, res) => {
   const { articlesId } = req.params;
   const comments = [];
   //code to get comments by articlesId
   res.json(comments);
 });
 
-app.post("/users", (req, res) => {
+indexRouter.post("/users", (req, res) => {
   const { email } = req.body;
   const userExists = users.find((u) => u.email === email);
   if (userExists) {
@@ -26,18 +26,18 @@ app.post("/users", (req, res) => {
   res.json(req.body);
 });
 
-app.post("/articles", (req, res) => {
+indexRouter.post("/articles", (req, res) => {
   // code to add a new article...
   res.json(req.body);
 });
 
-app.put("/articles/:id", (req, res) => {
+indexRouter.put("/articles/:id", (req, res) => {
   const { id } = req.params;
   // code to update an article...
   res.json(req.body);
 });
 
-app.delete("/articles/:id", (req, res) => {
+indexRouter.delete("/articles/:id", (req, res) => {
   const { id } = req.params;
   // code to delete an article...
   res.json({ deleted: id });
